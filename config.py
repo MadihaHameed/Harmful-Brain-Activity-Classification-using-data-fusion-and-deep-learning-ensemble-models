88cbc3bc30c1c3403b9f0574bbffdd0df82477c9
Madiha Hameed <52617926+MadihaHameed@users.noreply.github.com>
2025-08-27 12:40:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import torch
 
-CSV_PATH = "D:/marriam-ms-project/hms-harmful-brain-activity-classification/train.csv"
-EEG_DIR = "D:/marriam-ms-project/hms-harmful-brain-activity-classification/eeg_images"
-SPEC_DIR = "D:/marriam-ms-project/hms-harmful-brain-activity-classification/spectrogram_images"
+EEG_DIR = "D:/eeg_idata"
+SPEC_DIR = "D:/spectrogram_data"
 
 BATCH_SIZE = 16
 EPOCHS = 5
 LR = 0.001
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
